77f1a3c2f0aa42298fd987289e9203df1411be74
Albert Dedushi <adedushi@hotmail.com>
2023-12-15 17:13:26 -0500

    Add score counter

--- src/scripts/game-view.js
+++ src/scripts/game-view.js
@@ -6,6 +6,4 @@ class GameView {
 
 }
 
-
-
 export default GameView;
--- src/scripts/game.js
+++ src/scripts/game.js
@@ -4,10 +4,12 @@ const start = document.querySelector("#start");
 const productDisplay = document.querySelector('.product-display');
 const higherButton = document.querySelector('#higher')
 const lowerButton = document.querySelector('#lower')
+const scoreBoard = document.querySelector('#score')
 
 class Game {
     constructor(product) {
         this.product = product;
+        this.score = 0;
     }
 
     static generateURL() {
@@ -61,23 +63,31 @@ class Game {
 
     }
     guessHigher() {
-        return console.log(this.product.price >
-             this.product.placeHolderPrice);
+        this.product.price > this.product.placeHolderPrice ? this.correct() : this.incorrect()
     }
 
     guessLower() {
-        return console.log(this.product.price < 
-            this.product.placeHolderPrice);
+        this.product.price < this.product.placeHolderPrice ? this.correct() : this.incorrect()
+    }
+
+    correct(){
+        this.score++;
+        scoreBoard.innerHTML = `Score: ${this.score}`;
+    }
+
+    incorrect() {
+        this.score = 0;
+        scoreBoard.innerHTML = `Score: ${this.score}`
     }
 
 }
-
-
 
 let game = new Game()
 
 start.addEventListener('click', game.fetchProduct.bind(game));
 higherButton.addEventListener('click', game.guessHigher.bind(game));
+higherButton.addEventListener('click', game.fetchProduct.bind(game));
 lowerButton.addEventListener('click', game.guessLower.bind(game));
+lowerButton.addEventListener('click', game.fetchProduct.bind(game));
 
 export default Game;
