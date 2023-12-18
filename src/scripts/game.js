c8d3a9719473e8adf42c184bdbd161ff6a938ffb
adedushi <adedushi@hotmail.com>
2023-12-18 11:01:47 -0500

    Update game.js

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -25,7 +25,7 @@ class Game {
     static generateURL() {
         let product_id = Game.selectProduct()
         while (productHistory.indexOf(product_id) > -1) product_id = Game.selectProduct()
-        const API_KEY = "FTJXEwF95489m9I5HzWazZ0l";
+        const API_KEY = "";
         return `https://api.bestbuy.com/v1/products(sku=${product_id})?apiKey=${API_KEY}&sort=name.asc&show=name,image,salePrice,url,sku&format=json`
     }
 
